29c0dcdc116da5a1d66e33fa13f58235dd4786b1
XFLSWAN <158449266+XFLSWAN@users.noreply.github.com>
2024-12-24 14:29:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PHONE_AVIALBLE")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+M5JOTB8368NkNWM1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+Nr5X8QzLacA2NDFl")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+fllBBOC5ti03MTJl")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
